55a197b717adf131dc621b360d506c239f789769
Mariana <marianabatalhadosloios@gmail.com>
2024-03-14 00:06:59 +0000

    created first stage pages and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -8,28 +6,22 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <header>
+      <h1>Movies Stuff</h1>
+    </header>
+    <main>
+      Movies Carrousel goes here
+    </main>
+      <footer>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Here is the link to the{" "}
+          <a href="https://github.com/marianaGOD" target="_blank">
+            GitHub Mariana's GitHub
+          </a>
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </footer>
     </>
-  )
+  );
 }
 
 export default App
